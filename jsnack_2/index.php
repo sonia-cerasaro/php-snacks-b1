22119f15db2109e745b5edb8de9ac2d6ed473e0d
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-04-29 17:15:38 +0200

    jsnack 2 done

--- jsnack_2/index.php
+++ jsnack_2/index.php
@@ -11,16 +11,7 @@
     $email = $_GET['getMail'];
     $age = $_GET['getAge'];
 
-    // $emailValidate = filter_var($mail, FILTER_VALIDATE_EMAIL);
-    // $agevalidate = filter_var($age, FILTER_VALIDATE_INT)
-
-    // echo $name;
-    // echo $email;
-    // echo $age;
-    // var_dump($name);
-    // var_dump($ageNumber);
-
-    if (strlen($name)< 3) && (filter_var($email, FILTER_VALIDATE_EMAIL)) && (filter_var($age, FILTER_VALIDATE_INT)) {
+    if (strlen($name) > 3 && filter_var($email, FILTER_VALIDATE_EMAIL) && filter_var($age, FILTER_VALIDATE_INT)) {
     echo("I dati sono corretti.");
     } else {
     echo("C'e' un errore, inserisci di nuovo i dati.");
@@ -32,7 +23,7 @@
     // echo("$age is not a number");
     // }
 
-    // if (strlen($name)< 3) {
+    // if (strlen($name) > 3) {
     // echo("$name troppo corto");
     // } else {
     // echo("$name va bene");
